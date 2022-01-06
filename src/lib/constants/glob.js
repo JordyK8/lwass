551d19ce8abba4c8ab881ef5f990c6b741112db0
Jordy Kokelaar <66562183+JordyK8@users.noreply.github.com>
2022-01-06 13:42:58 +0100

    Update glob.js

--- src/lib/constants/glob.js
+++ src/lib/constants/glob.js
@@ -1,5 +1,6 @@
 module.exports = {
     MAX_VIDEO_SIZE: 1073741824,
     VIDEO_SCREENSHOT_TIME: "00:00:03",
+    VIDEO_FILE_EXTENSIONS: [".WEBM",".MPG," ,".MP2", ".MPEG", ".MPE", ".MPV" ,".OGG" ,".MP4",".M4P", ".M4V", ".AVI", ".WMV", ".MOV", ".QT", ".FLV", ".SWF"],
     
 }
