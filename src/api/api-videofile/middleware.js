261e9fee6506a34ac38c87891d5e87cd4f9de7f9
Jordy Kokelaar <66562183+JordyK8@users.noreply.github.com>
2022-01-06 13:42:29 +0100

    Update middleware.js

--- src/api/api-videofile/middleware.js
+++ src/api/api-videofile/middleware.js
@@ -1,4 +1,13 @@
 const constants = require('../../lib/constants/glob');
+
+const validateEmail = (email) => {
+  return String(email)
+    .toLowerCase()
+    .match(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
+};
+
 const middleware = {
   /**
    * Validates fromdata
@@ -14,13 +23,8 @@ const middleware = {
       ...req.files
     };
     let msg;
-    const validateEmail = (email) => {
-      return String(email)
-        .toLowerCase()
-        .match(
-          /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-        );
-    };
+    const fileExtention = data.file.name.substring(data.file.name.lastIndexOf('.'), data.file.name.length);
+    if(!constants.VIDEO_FILE_EXTENSIONS.includes(fileExtention)) msg = "File extension not supported.";
     if (!data.age) msg = 'Missing age from submition form.'
     if (!data.name || data.name.length < 2) msg = "Name invalid, note that given name must be at least two characters long.";
     if (!data.email || !validateEmail(data.email)) msg = "Given emailaddress seems to be invalid.";
